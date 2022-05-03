e2f5bee66f1a1feedfa4cd80dadd562b09bb1ac3
Muhammad Nizar <mnizarzr@gmail.com>
2022-05-03 23:14:07 +0700

    🚧 Add envar and connect to mongo & redis

---
+++ backend/config/app_config.py
@@ -0,0 +1,16 @@
+import os
+
+class AppConfig:
+    REDIS_HOST = os.getenv("REDIS_HOST", "localhost")
+    REDIS_PORT = os.getenv("REDIS_PORT", 6379)
+    REDIS_DB = os.getenv("REDIS_DB", 0)
+    REDIS_URL = f'redis://{REDIS_HOST}:{REDIS_PORT}/?db={REDIS_DB}'
+
+    MONGO_HOST = os.getenv("MONGO_HOST", "localhost")
+    MONGO_USER = os.getenv("MONGO_USER", "root")
+    MONGO_PWD = os.getenv("MONGO_PWD", "root")
+    MONGO_PORT = os.getenv("MONGO_PORT", 27017)
+    MONGO_DB = os.getenv("MONGO_DB", "test")
+
+    MONGO_URL = f'mongodb://{MONGO_USER}:{MONGO_PWD}@{MONGO_HOST}:{MONGO_PORT}/{MONGO_DB}?retryWrites=true&w=majority'
+    MOTOR_URI = MONGO_URL
--- backend/server.py
+++ backend/server.py
@@ -1,11 +1,41 @@
+import logging
+import os
+import aioredis
 from sanic import Sanic
 from sanic.response import text
+from sanic_motor import BaseModel
+
+from config.app_config import AppConfig
 
 app = Sanic(__name__)
+
+app.update_config(AppConfig)
+
+BaseModel.init_app(app)
 
 @app.get("/")
 async def hello_world(request):
     return text("Hello, world.")
 
+
+@app.before_server_start
+async def before_server_start(appop):
+    _redis_url = app.config.get("REDIS_URL")
+    try:
+        _redis = await aioredis.from_url(_redis_url)
+    except ConnectionError:
+        logging.error("Redis connection error")
+        app.stop()
+        exit(1)
+    setattr(app.ctx, "redis", _redis)
+
+
+@app.after_server_stop
+async def after_server_stop():
+    logging.info(f'[redis] closing connection')
+    app.ctx.redis.close()
+
 if __name__ == '__main__':
-    app.run(dev=True, debug=True, host="0.0.0.0")
+    app.run(debug=os.getenv("DEBUG", False),
+            host=os.getenv("HOST", "127.0.0.1"),
+            port=os.getenv("PORT", 8000))
